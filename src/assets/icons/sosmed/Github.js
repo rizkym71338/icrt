5ef6a33ad8831a43b88c6b65a535eec1ee768102
rizkym71338 <rizkym71338@gmail.com>
2022-04-20 08:55:14 +0700

    update dikit

--- src/assets/icons/sosmed/Github.js
+++ src/assets/icons/sosmed/Github.js
@@ -5,7 +5,7 @@ const Github = ({ width, height }) => {
     <svg
       width={width}
       height={height}
-      className="cursor-pointer text-slate-700 dark:text-slate-100 hover:text-primary-500 group-hover:text-primary-500 transition-300"
+      className="cursor-pointer text-slate-700 dark:text-slate-100 hover:text-primary-500 group-hover:text-primary-500 dark:hover:text-primary-500 transition-300"
       viewBox="0 0 24 25"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,5 +1,6 @@
 export { default as Logo } from "./logo.svg";
 export { default as LogoIcon } from "./logo-icon.svg";
+export { default as Avatar } from "./Avatar.svg";
 export { default as Blob } from "./Blob";
 export { default as BgCardStructure } from "./BgCardStructure.js";
 export * from "./teams";
